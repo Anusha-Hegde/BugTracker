2b489b4ae536b086f8511101ae0b96a63c9d1797
Anusha Hegde <anushaprhegde@gmail.com>
2020-08-30 16:48:26 +0530

    model update

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import Project, Issue
+from .models import Project, Issue, Thread, Tag, ProjectMember
 
 # Register your models here.
 admin.site.register(Project)
+admin.site.register(Tag)
 admin.site.register(Issue)
+admin.site.register(Thread)
+admin.site.register(ProjectMember)
--- main/models.py
+++ main/models.py
@@ -1,27 +1,48 @@
 from django.db import models
 import datetime
+from django.contrib.auth.models import User, Group
+
+class Tag(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name 
 
 class Project(models.Model):
     name = models.CharField(max_length=50)
     desc = models.CharField(max_length=250)
     start = models.DateField(default=datetime.date.today())
-    end = models.DateField(blank = True, null = True)
+    end = models.DateField(null = True, blank = True)
     issues_open = models.IntegerField(null=False, blank=False, default=0)
     total_issues = models.IntegerField(null=False, blank=False, default=0)
     issues_closed = models.IntegerField(null=False, blank=False, default=0)
 
-
     def __str__(self):
         return self.name
+
 
 class Issue(models.Model):
     title = models.CharField(max_length=30)
     desc = models.CharField(max_length=30)
-    # proj = models.On(field='id', to=Project, field_name='id')
-    # (Project, on_delete=models.CASCADE, to_field='id')
-    proj = models.ForeignKey(Project, to_field='id', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, to_field='id', on_delete=models.SET_DEFAULT, default = 1) #default pointing to anonymous user
+    project = models.ForeignKey(Project, to_field='id', on_delete=models.CASCADE)
     category = models.CharField(max_length=30)
     priority = models.CharField(max_length=30)
-
+    is_open = models.BooleanField(default=True)
+    tags = models.ManyToManyField(Tag)
+    
     def __str__(self):
         return self.title
+
+
+class Thread(models.Model):
+    user = models.ForeignKey(User, to_field='id', on_delete=models.SET_DEFAULT, default = 1)
+    issue = models.ForeignKey(Issue, to_field='id', on_delete=models.CASCADE)
+    comment = models.CharField(max_length=250)
+    upvote = models.IntegerField(default=0)
+
+
+class ProjectMember(models.Model):
+    user = models.ForeignKey(User, to_field='id', on_delete=models.CASCADE)
+    role = models.ForeignKey(Group, to_field='name', on_delete=models.CASCADE)
+    project = models.ForeignKey(Project, to_field='id', on_delete=models.CASCADE)
